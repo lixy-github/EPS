5dca07e3f9661bc9e3a456b24f6d374cb1cf2654
G.Y <lixy@broadcontact.com>
2019-03-18 17:33:39 +0800

    添加图片验证码，修改注册验证。

--- src/api/login.js
+++ src/api/login.js
@@ -1,14 +1,17 @@
 import axios from '../libs/AxiosPlugin'
 import qs from 'qs'
 
-//用户信息IPA（手机注册） 
-export const registerP = (data) => { return axios.post("/api/user/public/register/phone",data).then(res => res.data)}
+// 手机注册
+export const registerPhone = (data) => { return axios.post("/api/user/public/register/phone",data).then(res => res.data)}
 
-//用户信息API（邮箱注册）
-export const emailP = (data) => {return axios.post("/api/user/public/register/email",data).then(res => res.data)}
+// 邮箱注册
+export const registerEmail = (data) => {return axios.post("/api/user/public/register/email",data).then(res => res.data)}
 
-//用户信息IPA（用户名是否可用） 
-export const register = (params) => {return axios.get(`/api/user/public/register?${qs.stringify(params)}`).then(res => res.data) }
+// （注册时使用）验证用户名是否可用
+export const usernameValidate = (params) => {return axios.get(`/api/user/public/register?${qs.stringify(params)}`).then(res => res.data) }
 
 //authentication-rest-controller（auth）
 export const login = (data) => {return axios.post(`/auth`, data).then(res => res.data) }
+
+// 图片验证码
+export const ImageVerification = (data) =>{return axios.get(`/api/public/imageCode?${qs.stringify(data)}`).then(res => res.data) }
